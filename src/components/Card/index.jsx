9dcd922af00f7af69e53ed97978b5a6d9a08acc4
Dialtor <diepresiga@gmail.com>
2022-06-04 09:56:38 -0500

    Style Cards

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,27 +1,15 @@
 import style from './card.module.css';
 import iconCompany from '../../assets/svg/company.svg';
 import iconEmail from '../../assets/svg/email.svg';
-// import imgSrc from '../../assets/img/avatar'
 
 
-export default function Card({ username, email, name, company, images }) {
 
 
-  // let counter = 0;
-  // const newArray = images.map(element  => {
-  //   counter = counter + 1;
-  //   element.src == "" ? {...images, src: `../../assets/img/avatar${counter}.jpg`} : element
-  // })
-
-  // console.log(newArray)
-
-
+export default function Card({ username, email, name, company, src}) {
   return (
     <div className={style.card} id="selector-1">
       <div className={style.imgBox}>
-        <img src={images} alt="profile" />
-        {/* https://cloupyblob.blob.core.windows.net/cloupy/image-not-found.png */}
-        {/* https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 */}
+        <img src={src} alt="profile" />
         <div className={style.addImage}>
           <img src="https://freeiconshop.com/wp-content/uploads/edd/upload-cloud-outline.png" alt="" />
         </div>
--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -2,11 +2,13 @@ import style from './home.module.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Card from '../../components/Card';
+import './swiper-bundle.min.css';
 
 
 const Home = () => {
 
   const [data, setData] = useState([]);
+  const [dataWithImage, setDataWithImage] = useState([]);
 
   useEffect(() => {
     getData();
@@ -19,29 +21,56 @@ const Home = () => {
   const getData = async () => {
     axios.get(API).then((res) => {
       setData(res.data);
-      // console.log(data[0])
+
+      if (res.data.length>0) {
+        getImages(res.data);
+      }
+
     }).catch((err) => {
       console.log(err)
     });
   }
 
-  const images = [];
-for (let i = 1; i <= 10; i++) {
-  // const element = images.src = `../../assets/img/avatar${i}.jpg`;
-  images.push(`../../assets/img/avatar${i}.jpg`);
-}
 
-console.log(images)
+  const getImages = (data) => {
+    const images = [
+      'https://images.pexels.com/photos/3775125/pexels-photo-3775125.jpeg',
+      'https://images.pexels.com/photos/3769706/pexels-photo-3769706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      'https://images.pexels.com/photos/12192379/pexels-photo-12192379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      'https://images.pexels.com/photos/3772519/pexels-photo-3772519.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      'https://images.pexels.com/photos/3785077/pexels-photo-3785077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      'https://images.pexels.com/photos/1059180/pexels-photo-1059180.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      'https://images.pexels.com/photos/3785424/pexels-photo-3785424.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      'https://images.pexels.com/photos/3781545/pexels-photo-3781545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      'https://images.pexels.com/photos/3763152/pexels-photo-3763152.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    ];
 
+      let newData = data.map(value => {
+        return (
+          images.map(image => {
+            return (
+              {...value, src: image}
+            )
+          })
+        )
+      })
+
+      setDataWithImage(newData[0]);
+  }
+
+  console.log("JSON With Image Data",dataWithImage);
   return (
     <div className={style.cards}>
-      {data && data.map((user) => {
+      {dataWithImage && dataWithImage.map((user,index) => {
           return (
             <Card
+              key={index}
               name={user.name}
               username = {user.username}
               email = {user.email}
               company = {user.company.name}
+              src = {user.src}
             />
           )
         })
